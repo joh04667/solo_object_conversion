0c0b91deb9a17622af853b077c900a74ebd75253
Kyle Johnson <user@Users-MacBook-Pro.local>
2016-03-25 14:29:14 -0500

    Updates moneyReadable to display cents after decimal. Adds whitespace for readability.

--- objectapp.js
+++ objectapp.js
@@ -21,17 +21,20 @@ function Character(name, empNum, salary, rating) {
   	return [this.name, (this.sti() * 100).toString() + '%' , moneyReadable(parseInt(this.salary) + (this.salary * this.sti())), Math.round(this.salary * this.sti())];
  };
 }
+
 // Object construction
 var atticus = new Character("Atticus", "2405", "47000", 3);
 var jem = new Character("Jem", "62347", "63500", 4);
 var boo = new Character("Boo", "11435", "54000", 3);
 var scout = new Character("Scout", "6243", "74750", 5);
+
 // Object array
 var employees = [atticus, jem, boo, scout];
+
 // Call totalComp method on each array item
 employees.forEach(s => console.log(s.totalComp()));
+
 // Global function to format salary amount
 function moneyReadable(num) {
-   return "$" + num.toLocaleString({style: 'currency', currency: 'USA'});
+   return "$" + num.toLocaleString('en-US',{style: 'currency', currency: 'USD'});
 }
-//TODO: Display salary in proper currency format with two cents after the decimal.
